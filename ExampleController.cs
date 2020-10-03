1901cc06e819621ebc959b59fb7bac59d3a6b1df
Ria Singh <35395601+riasingh16@users.noreply.github.com>
2020-10-03 16:08:52 +0530

    Add files via upload

---
+++ ExampleController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ViewResult AllEmployees()
+        {
+            var context = new LttsEntities();
+            var model = context.EmpTables.ToList();
+            return View(model);
+        }
+        public ViewResult Find(string id)
+        {
+            int empID = int.Parse(id);
+            var context = new LttsEntities();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == empID);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Find(EmpTable emp)
+        {
+            var context = new LttsEntities();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == emp.EmpID);
+            model.EmpName = emp.EmpName;
+            model.EmpSalary = emp.EmpSalary;
+            context.SaveChanges();
+            return RedirectToAction("AllEmployees");
+        }
+        
+        public ViewResult NewEmployee()
+        {
+            var model = new EmpTable();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult NewEmployee(EmpTable emp)
+        {
+            var context = new LttsEntities();
+            context.EmpTables.Add(emp);
+            context.SaveChanges();
+            return RedirectToAction("AllEmployees");
+        }
+            
+
+
+    }
+}
---
+++ MyData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class MyData
+    {
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+
+        public override string ToString()
+        {
+
+            return $"The EmployeeID:{EmployeeID}<p>The name:{EmployeeName}<p>The Employee Address:{EmployeeAddress}<p><p>";
+        }
+
+
+    }
+}
